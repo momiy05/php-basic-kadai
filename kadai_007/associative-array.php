a19c8be4bdc8c738f47dc1bafe88d0b3163642e9
momiy05 <127914793+momiy05@users.noreply.github.com>
2024-10-08 23:37:56 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>kadai_007</title>
+    </head>
+    <body>
+        <p>
+            <?php
+                $array = ["name" => "onion","price" => 200,"weight" => 160];
+                print_r($array);
+            ?>
+        </p>
+    </body>
+</html>
